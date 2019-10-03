be9de9780a078afc4ecffc1779131d5ac6ede4f7
ErmiP <ermi.pristiya@gmail.com>
2019-10-03 16:26:56 +0700

    commit1,2

--- app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/MainActivity.java
@@ -7,26 +7,63 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import id.ac.polinema.idealbodyweight.fragments.AboutFragment;
+import id.ac.polinema.idealbodyweight.fragments.BrocaIndexFragment;
+import id.ac.polinema.idealbodyweight.fragments.MenuFragment;
 
-	// Deklarasikan atribut Fragment di sini
+public class MainActivity extends AppCompatActivity implements MenuFragment.OnFragmentInteractionListener, BrocaIndexFragment.OnFragmentInteractionListener {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
+    // Deklarasikan atribut Fragment di sini
+    private AboutFragment aboutFragment;
+    private MenuFragment menuFragment;
+    private BrocaIndexFragment brocaIndexFragment;
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.menu, menu);
-		return  true;
-	}
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	@Override
-	public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-		// TODO: Tambahkan penanganan menu di sini
+        aboutFragment = AboutFragment.newInstance("Ermi Pristiyaingurum");
+        menuFragment = new MenuFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, menuFragment)
+                .commit();
 
-		return super.onOptionsItemSelected(item);
-	}
+        brocaIndexFragment = new BrocaIndexFragment();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // TODO: Tambahkan penanganan menu di sini
+        if (item.getItemId() == R.id.menu_about) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, aboutFragment)
+                    .addToBackStack(null)
+                    .commit();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBrocaIndexButtonClicked() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, brocaIndexFragment)
+                .commit();
+    }
+
+    @Override
+    public void onBodyMassIndexButtonClicked() {
+
+    }
+
+    @Override
+    public void onCalculateBrocaIndexClicked(float index) {
+
+    }
 }
